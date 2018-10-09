7d52877327583100e4334a6ca58c001646f2c063
Farhan Asif <f.asif2010@gmail.com>
2018-10-09 12:48:40 +0600

    sync localhost

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Another glitch in paradise!'))
+app.get('/', (req, res) => res.send('We have synced glitch, github and localhost environment!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
